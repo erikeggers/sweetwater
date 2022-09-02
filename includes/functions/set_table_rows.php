bf54c5968b0638eb59bf57e725289d5d339b6a97
Erik Eggers <em.eggers@gmail.com>
2022-09-02 10:16:33 -0400

    Moves setTablesRows out of table view for future reusability

---
+++ includes/functions/set_table_rows.php
@@ -0,0 +1,36 @@
+<?php
+// Get data from database based on query
+function setTableRows($query)
+{
+  include 'includes/dbh.inc.php';
+
+  // Query array
+  $queryArray = array(
+    "candy" => "SELECT * FROM sweetwater_test WHERE comments like '%candy%' OR comments like '%smarties%' OR comments like '%taffy%'",
+    // Noticed some spanish words in the comments, so added Spanish words to the query to catch those as well.
+    "calls" => "SELECT * FROM sweetwater_test WHERE (comments like '% call %' OR comments like '% call.%' OR comments like '% calls%' OR comments like '%comunicarse%' OR comments like '%llámame%') AND comments not like '% call your%'",
+    "referrals" => "SELECT * FROM sweetwater_test WHERE comments like '%referred%' OR comments like '%referral%'",
+    "signatureRequirements" => "SELECT * FROM sweetwater_test WHERE comments like '%signature%' OR comments like '%sign%' OR comments like '%release%'",
+    "miscellaneous" => "SELECT * FROM sweetwater_test WHERE comments not in (SELECT comments FROM sweetwater_test WHERE comments like '%candy%' or comments like '%smarties%' or comments like '%taffy%' OR comments like '% call %' OR comments like '% call.%' OR comments like '% calls%' OR comments like '%comunicarse%' OR comments like '%llámame%' OR comments like '%referred%' or comments like '%referral%' OR comments like '%signature%' OR comments like '%sign%' OR comments like '%release%')"
+  );
+
+
+  $queryToUse = $queryArray[$query];
+
+  $result = $conn->query($queryToUse);
+
+  // Check if query was successful
+  if (!$result) {
+    die("Query failed: " . $conn->error);
+  }
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+      echo "<tr><td>" . $row["orderid"] . "</td><td>" . $row["comments"] . "</td><td>" . $row["shipdate_expected"] . "</td><tr>";
+    }
+  } else {
+    echo "No results found";
+  }
+  $conn->close();
+}
